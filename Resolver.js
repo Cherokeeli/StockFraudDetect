be0e8904dcae5d7aaebb8b14a1b0a68d4d1aeb90
QUAN Li <quanlime@gmail.com>
2017-04-26 22:00:37 +0800

    combine with increase rate in 5 days shift

--- Resolver.js
+++ Resolver.js
@@ -1,10 +1,11 @@
 var fs = require('fs');
 var stringify = require('csv-stringify');
 
-var t_result = fs.readFileSync('../result.json'); //refer news source
-var t_dict = fs.readFileSync('./dic.json'); //refer dictiony source
-var t_stocks = fs.readFileSync('./stocks.json'); //refer stock data
-var result = JSON.parse(t_result);
+//var t_result = fs.readFileSync('../result.json'); //refer news source
+var t_dict = fs.readFileSync('./data/dic.json'); //refer dictiony source
+var t_stocks = fs.readFileSync('./data/stocks.json'); //refer stock data
+var t_volume =fs.readFileSync('./data/stocks_volume.json');
+//var result = JSON.parse(t_result);
 var dict = JSON.parse(t_dict); //convert to json
 var stocks = JSON.parse(t_stocks);
 
@@ -44,21 +45,36 @@ Date.prototype.diff = function (date) {
 }
 
 function stockPriceFromData(sid, time, offset) {
-    var x_offset = stocks[0].indexOf(sid), price;
-    //console.log(sid, x_offset);
-    var origin_date = new Date('2016-03-15');
-    time = new Date(time);
-    var y_offset = 5 * Math.floor(time.diff(origin_date)) / 7;
-    //console.log('x: '+Math.floor(y_offset + offset - 12));
-
-    if ((y_offset + offset - 12) <= 249 && stocks[Math.floor(y_offset + offset - 13)][x_offset]) {
-        //console.log('finding: '+stocks[Math.floor(y_offset + offset - 13)][0]);
-        price = stocks[Math.floor(y_offset + offset - 13)][x_offset];
+    console.log("offset"+offset);
+    var x_offset = stocks[0].indexOf(sid),price=0;
+    offset = offset*(24*60*60*1000);
+    var set_time = new Date((new Date(time)).getTime()+offset);
+    // console.log("comptime ",set_time.getFullYear(),set_time.getMonth(),set_time.getDate());
+    //var comp_time = new Date(stocks[6][0]);
+    //console.log(set_time.getDay(),comp_time.getDay());
+    for (var i = 2; i < stocks.length; i++) {
+        var comp_time = new Date(new Date(stocks[i][0]));
+       // console.log("comptime ",set_time.getFullYear(),comp_time.getFullYear(),set_time.getMonth(),comp_time.getMonth(),set_time.getDate(),comp_time.getDate());
+        if ((set_time.getFullYear()==comp_time.getFullYear())&&(set_time.getMonth()==comp_time.getMonth())&&(set_time.getDate()==comp_time.getDate())) {
+            console.log("Now the time is "+stocks[i][0]);
+            //console.log("comptime ",set_time.getFullYear(),comp_time.getFullYear(),set_time.getMonth(),comp_time.getMonth(),set_time.getDay(),comp_time.getDay());
+            return stocks[i][x_offset];
+        }
     }
-    else
-        price = 0;
+    // var origin_date = new Date('2016-03-15');
+    // time = new Date(time);
+    // var y_offset = 5 * Math.ceil(time.diff(origin_date) / 7 ) + Math.ceil(time.diff(origin_date)%7);
+    // console.log('diff: '+y_offset);
+    // console.log(y_offset + offset - 18,x_offset);
+    // if ((y_offset + offset - 18) <= 249 && typeof stocks[Math.floor(y_offset + offset - 18)][x_offset] != 'undefined') {
+    //     //console.log('finding: '+stocks[Math.floor(y_offset + offset - 13)][0]);
+    //     price = stocks[Math.floor(y_offset + offset - 18)][x_offset];
+    //     console.log("Now the time is "+stocks[Math.floor(y_offset + offset-18)][0]);
+    // }
+    // else
+    //     price = 0;
     //console.log('price: '+price+'y: '+Math.floor(y_offset + offset-10));
-    return parseFloat(price); //return stock price
+    //return parseFloat(price); //return stock price
 }
 
 function rateOfRecommand(up, down) {
@@ -69,110 +85,177 @@ function zeroPadding(num, n) { //add prefix zero with total n
     return Array(n - ('' + num).length + 1).join(0) + num;
 }
 
-function resolveSentence() {
+function resolveSentence(result, callback) {
 
     var re = /([^\u3002]*[\uff08]\d+[\uff09][^\u3002]*)[\u3002]/g; //根据股票号码所在的句子进行断句
     var pe = /[\uff08]\d+[\uff09]/g;
     var extract_result = [];
-    for (var i = 0; i < result.length - 1; i++) { // number of news loop
 
-        var matchs, sentences = [], submatch, recom_listrate = [], sentence_rate = [];
-        var increase_num = 0, total_len; //count for increase stocks
-        // if (result[i].time=='2017-03-15')
-        //     continue;
-        matchs = re.exec(result[i].content);
-        if ((result[i].content.match(pe)) == null) {
-            result[i].rise = 0;
-            result[i].drop = 0;
-            continue; // if no stock id; continue loop
+    var matchs, sentences = [], submatch, recom_listrate = [], sentence_rate = [];
+    var increase_num = 0, total_len; //count for increase stocks
+    // if (result[i].time=='2017-03-15')
+    //     continue;
+    matchs = re.exec(result.content);
+    if ((result.content.match(pe)) == null) {
+        result.rise = 0;
+        result.drop = 0;
+        return; // if no stock id; continue loop
+    }
+    var ori_date = new Date('2017-03-15');
+    var now_date = new Date(result.time);
+    //console.log("ori:%s, now:%s",ori_date,now_date)
+    if (now_date.getTime() == ori_date.getTime())
+        return;
+
+    total_len = result.content.match(pe).length;
+    while (matchs != null) { //match the substring
+        var count = (matchs[0].match(pe)).length; //have many id in the same substring
+        sentences.push(matchs[0]);
+
+        for (var x = 0; x < count - 1; x++) {
+            sentences.push(matchs[0]); // added the same substring in result
         }
-        var ori_date = new Date('2017-03-15');
-        var now_date = new Date(result[i].time);
-        //console.log("ori:%s, now:%s",ori_date,now_date)
-        if (now_date.getTime() == ori_date.getTime())
-            continue;
+        matchs = re.exec(result.content); //contine match next substring
+    }
 
-        total_len = result[i].content.match(pe).length;
-        while (matchs != null) { //match the substring
-            var count = (matchs[0].match(pe)).length; //have many id in the same substring
-            sentences.push(matchs[0]);
-
-            for (var x = 0; x < count - 1; x++) {
-                sentences.push(matchs[0]); // added the same substring in result
+    for (var j = 0; j < sentences.length; j++) { // number of sentences loop
+        var numOfDown = sentences[j].appearNumOfArray(dict[1], 1);
+        var numOfUp = sentences[j].appearNumOfArray(dict[0], 0);
+        var rate = rateOfRecommand(numOfUp[0], numOfDown[0]);
+        // result[i].rise = numOfUp;
+        // result[i].drop = numOfDown;
+        //console.log('rate: '+rate);
+        var ids = sentences[j].match(pe).map(e => {
+            console.log(e);
+            if (e.slice(1, -1).length>5) {
+                return e.slice(1, -1);
             }
-            matchs = re.exec(result[i].content); //contine match next substring
+            return zeroPadding(e.slice(1, -1), 5).concat('.HK'); // remove '（）' add.HK
+        }) //get id of stocks
+        //console.log(ids);
+        sentence_rate.push(rate);
+        var time = new Date(result.time), offset_from, offset_to, offset_to2, offset_to3, offset_to4, offset_to5;
+        console.log("time "+time.getDay());
+        if (time.getDay() == 6) {
+            offset_from = -1;
+            offset_to = 2;
+            offset_to2 = 3;
+            offset_to3 = 4;
+            offset_to4 = 5;
+            offset_to5 = 6;
+        } else if (time.getDay() == 0) {
+            offset_from = -2;
+            offset_to = 1;
+            offset_to2 = 2;
+            offset_to3 = 3;
+            offset_to4 = 4;
+            offset_to5 = 5;
+        } else if (time.getDay() == 5) {
+            offset_from= 0;
+            offset_to = 3;
+            offset_to2 = 4;
+            offset_to3 = 5;
+            offset_to4 = 6;
+            offset_to5 = 7;
+        } else if (time.getDay() == 1) {
+            offset_from= 0;
+            offset_to = 1;
+            offset_to2 = 2;
+            offset_to3 = 3;
+            offset_to4 = 4;
+            offset_to5 = 7;
+        } else if (time.getDay() == 2) {
+            offset_from= 0;
+            offset_to = 1;
+            offset_to2 = 2;
+            offset_to3 = 3;
+            offset_to4 = 6;
+            offset_to5 = 7;
+        } else if (time.getDay()==3) {
+            offset_from= 0;
+            offset_to = 1;
+            offset_to2 = 2;
+            offset_to3 = 5;
+            offset_to4 = 6;
+            offset_to5 = 7;
+        } else if (time.getDay()==4) {
+            offset_from= 0;
+            offset_to = 1;
+            offset_to2 = 4;
+            offset_to3 = 5;
+            offset_to4 = 6;
+            offset_to5 = 7;
         }
+        //if (rate > possibility) { //word evaluation possibili ty
+        for (var n = 0; n < ids.length; n++) { // 
+            console.log('idsn',ids[n]);
+            var t1 = stockPriceFromData(ids[n], result.time, offset_from)//time t1
+            var t2 = stockPriceFromData(ids[n], result.time, offset_to);//t+1
+            var t3 = stockPriceFromData(ids[n], result.time, offset_to2);//t+2
+            var t4 = stockPriceFromData(ids[n], result.time, offset_to3);//t+3
+            var t5 = stockPriceFromData(ids[n], result.time, offset_to4);//t+4
+            var t6 = stockPriceFromData(ids[n], result.time, offset_to5);//t+5
 
-        for (var j = 0; j < sentences.length; j++) { // number of sentences loop
-            var numOfDown = sentences[j].appearNumOfArray(dict[1], 1);
-            var numOfUp = sentences[j].appearNumOfArray(dict[0], 0);
-            var rate = rateOfRecommand(numOfUp[0], numOfDown[0]);
-            // result[i].rise = numOfUp;
-            // result[i].drop = numOfDown;
-            //console.log('rate: '+rate);
-            var ids = sentences[j].match(pe).map(e => {
-                return zeroPadding(e.slice(1, -1), 5).concat('.HK'); // remove '（）' add.HK
-            }) //get id of stocks
-            //console.log(ids);
-            sentence_rate.push(rate);
-            var time = new Date(result[i].time), offset_from = 0, offset_to = 1;
-            if (time == 6) {
-                offset_from = -1;
-                offset_to = 2;
-            } else if (time == 0) {
-                offset_from = -2;
-                offset_to = 1
+            var inc_ratio = t1 == 0 ? 0 : (t2 - t1) / t1;
+            var inc_ratio2 = t1 == 0 ? 0 : (t3 - t1) / t1;
+            var inc_ratio3 = t1 == 0 ? 0 : (t4 - t1) / t1;
+            var inc_ratio4 = t1 == 0 ? 0 : (t5 - t1) / t1;
+            var inc_ratio5 = t1 == 0 ? 0 : (t6 - t1) / t1;
+            if (inc_ratio == -1)
+                console.log("t1:%s, t2:%s, inc:%s, id:%s, date:%s", t1, t2, inc_ratio, ids[n], time);
+            //result[i].increase_rate = (t2 - t1) / t1;
+            if (inc_ratio >= increase_rate) { //calculate premium rate
+                increase_num++;
             }
-            //if (rate > possibility) { //word evaluation possibility
-            for (var n = 0; n < ids.length; n++) { // 
-                var t1 = stockPriceFromData(ids[n], result[i].time, offset_from)//time t1
-                var t2 = stockPriceFromData(ids[n], result[i].time, offset_to);//t+1
-
-                var inc_ratio = t1 == 0 ? 0 : (t2 - t1) / t1;
-                if (inc_ratio == -1)
-                    console.log("t1:%s, t2:%s, inc:%s, id:%s, date:%s", t1, t2, inc_ratio, ids[n], time);
-                //result[i].increase_rate = (t2 - t1) / t1;
-                if (inc_ratio >= increase_rate) { //calculate premium rate
-                    increase_num++;
-                }
-                var element = [
-                    sentences[j],
-                    ids[n],
-                    result[i].time,
-                    numOfUp[0],
-                    numOfDown[0],
-                    inc_ratio,
-                    result[i].author
-                ];
-                //element.concat()
-                //console.log(numOfUp[1])
-                extract_result.push(element.concat(numOfUp[1]).concat(numOfDown[1]));
-            }// for ids
-            //} // if should find price
-            //console.log(sentences[j] + ': ' + numOfDown + ':' + numOfUp);
-        }// for sen
-        recom_listrate.push(sentence_rate);
-        //console.log(increase_num/ total_len);
-        //recom_listrate[i].acc_rate = increase_num/(total_len);
-        if (increase_num / (total_len) >= accurate_rate) {
-            console.log('CAUTION! Suspected Case in ' + result[i].pubName + ', From ' + result[i].author + ', With accurate of ' + increase_num / (total_len));
-        }
-    }//for news
+            var element = [
+                sentences[j],
+                ids[n],
+                result.pubName,
+                result.time,
+                numOfUp[0],
+                numOfDown[0],
+                inc_ratio,
+                inc_ratio2,
+                inc_ratio3,
+                inc_ratio4,
+                inc_ratio5,
+                t1,
+                t2,
+                t3,
+                t4,
+                t5,
+                t6,
+                result.author
+            ];
+            //element.concat()
+            //console.log(numOfUp[1])
+            extract_result.push(element.concat(numOfUp[1]).concat(numOfDown[1]));
+        }// for ids
+        //} // if should find price
+        //console.log(sentences[j] + ': ' + numOfDown + ':' + numOfUp);
+    }// for sen
+    recom_listrate.push(sentence_rate);
+    //console.log(increase_num/ total_len);
+    //recom_listrate[i].acc_rate = increase_num/(total_len);
+    // if (increase_num / (total_len) >= accurate_rate) {
+    //     console.log('CAUTION! Suspected Case in ' + result.pubName + ', From ' + result.author + ', With accurate of ' + increase_num / (total_len));
+    // }
+    callback(extract_result);
 }
 exports.resolveSentence = resolveSentence;
 
-var heading = ['descriptions', 'stock', 'time', 'rise', 'drop', 'increase_rate', 'author'].concat(dict[0]).concat(dict[1]) // dictiony notation added
-extract_result.unshift(heading); //added heading to the top of the prepare csv array
+
+//extract_result.unshift(heading); //added heading to the top of the prepare csv array
 
 
 //input = [ [ '1', '2', '3', '4' ], [ 'a', 'b', 'c', 'd' ] ];
-stringify(extract_result, function (err, output) {
-    fs.open('./extract_resultTEST.csv', 'w+', function (err, fd) {
-        fs.write(fd, output, 0, function (err, bytes) {
-            console.log("Successful Write");
-        });
-    });
-});
+// stringify(extract_result, function (err, output) {
+//     fs.open('./extract_resultTEST.csv', 'w+', function (err, fd) {
+//         fs.write(fd, output, 0, function (err, bytes) {
+//             console.log("Successful Write");
+//         });
+//     });
+// });
 
 //var format = JSON.stringify(result);
 
--- largeJSON.js
+++ largeJSON.js
@@ -2,17 +2,47 @@ var fs = require('fs'),
     JSONStream = require('JSONStream'),
     es = require('event-stream'),
     fs = require('fs'),
-    stringify = require('csv-stringify');
-    rj = require('Resolver');
+    stringify = require('csv-stringify'),
+    rj = require('./Resolver');
+var t_dict = fs.readFileSync('./dic.json'); //refer dictiony source
+//var result = JSON.parse(t_result);
+var dict = JSON.parse(t_dict);
 
 var getStream = function () {
-    var jsonData = '../result_final.json',
+    var jsonData = '../result_final.json', // input json file
         stream = fs.createReadStream(jsonData, { encoding: 'utf8' }),
         parser = JSONStream.parse('*');
     return stream.pipe(parser);
 };
 
-getStream()
-    .pipe(es.mapSync(function (data) {
-        console.log(data.author);
-    }));
+var setheading = function () {
+    var heading = ['descriptions', 'stock', 'publisher', 'time', 'rise', 'drop', 'increase_rate_t+1', 'increase_rate_t+2', 'increase_rate_t+3', 'increase_rate_t+4', 'increase_rate_t+5','t0','t1','t2','t3','t4','t5', 'author'].concat(dict[0]).concat(dict[1]) // dictiony notation added
+    // try{
+    // heading = heading.toString();
+    // } catch (err){ console.log(err)}
+    console.log('setheading')
+    stringify(heading, function (err, output) {
+        fs.open('./stocknews.csv', 'a+', function (err, fd) {
+            fs.write(fd, output, 0, function (err, bytes) {
+                console.log("Successful Write HEADING");
+                getStream()
+                    .pipe(es.mapSync(function (data) {
+                        if (data.time=='2016-03-14') return;
+                        rj.resolveSentence(data, chunk => {
+                            stringify(chunk, function (err, output) {
+                                fs.open('./stocknews.csv', 'a+', function (err, fd) {
+                                    fs.write(fd, output, 0, function (err, bytes) {
+                                        console.log("Successful Write DATA");
+                                    });
+                                });
+                            });
+                        });
+                        //console.log(data.author);
+
+                    }));
+            });
+        });
+    });
+}
+
+setheading();
